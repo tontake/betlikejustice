7c16f1eef2aba0433a240d86d65b97cae9ca3191
tontake <graceglory591@gmail.com>
2019-06-09 13:56:26 -0700

    added first page

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+<head><title>Justice Mpofu</title>
+<style>
+
+.content{
+	border-radius:10px;
+	background-color:#eee;
+	border:solid 1px yellow;
+	padding:10px;
+	
+	font-family:serif;
+	
+}
+</style>
+
+</head>
+<body>
+<h2 align="center">
+<?php
+ echo "WELCOME TO JUSTICE MPOFU MATCH TACTICAL ADVISARY SITE";
+
+
+
+?>
+</h2>
+<br>
+<div class="content">
+<p>He is a very good football match analyst who started betting at an early age .He has predicted more than 200 matches.His skills have developed by leaps and bounds during his time from when he started knowing soccer football.<br>
+A passionate young vibrant man aged 26 in the time of writing has helped betting amateur to love betting and be better football analyst .He takes betting as a hobby not as a source income but as a sport .Since he loves football ,once a  footballer player ,he is dedicated to helping  other upcoming footballers through impacting them with knowledge he has gained throughout his football  tenure.</p>
+</div>
+<h2 color="blue"><b>Soccer Fan<b></h2>
+
+<small><i>JUSTICE MPOFU</i></small> 
+
+</br></br>
+<hr>
+<h3 color="red" align="center">WEBSITE STILL UNDER CONSTRUCTION</h3>
+<body>
+</html>
